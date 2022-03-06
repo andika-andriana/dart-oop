69ecb5f8c96d94724d40cab1c5cd88bf9e0abce7
Andika Andriana <andriana.mia1@gmail.com>
2022-03-06 09:01:38 +0700

    + Getter setter example

---
+++ bin/data/car.dart
@@ -0,0 +1,15 @@
+abstract class Car{
+  String name = '';
+
+  void drive(){
+
+  }
+
+  int getTire(){
+    return 0;
+  }
+}
+
+abstract class HasBrand{
+  String getBrand();
+}
---
+++ bin/interface.dart
@@ -0,0 +1,27 @@
+import 'data/car.dart';
+
+/// pada Dart interface dibuat menggunakan kata implements
+/// berbeda dengan bahasa programming lain, Dart tidak memiliki kata interface
+/// jadi interface dibuat menggunakan kata implements menggunakan metode inheritance
+/// berbeda dengan extends yang hanya dapat mencantumkan 1 parent
+/// implements dapat mencantumkan lebih dari 1 parent
+
+class Avanza implements Car{
+  String name = 'Avanza';
+
+  void drive() {
+    print('Avanza is running');
+  }
+
+  int getTire() {
+    return 4;
+  }
+
+}
+
+void main(){
+  var av = Avanza();
+  print(av.name);
+  av.drive();
+  print(av.getTire().toString());
+}
