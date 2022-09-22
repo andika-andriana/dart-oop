70f370c26c8838d26d01f2158a2b57b189857bda
Andika Andriana <andriana.mia1@gmail.com>
2022-09-22 09:05:31 +0700

    + Static field Example

---
+++ bin/data/app_static_field.dart
@@ -0,0 +1,4 @@
+class Application {
+  static final String name = 'PBO Dart SDK';
+  static final String author = 'Andika Andriana';
+}
---
+++ bin/static_field.dart
@@ -0,0 +1,6 @@
+import 'data/app_static_field.dart';
+
+void main(){
+  print('Application.name = ${Application.name}');
+  print('Application.author = ${Application.author}');
+}
